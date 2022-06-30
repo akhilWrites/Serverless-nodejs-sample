1babf4c16facda4a38b28933b86b4bac062c2baa
akhilWrites <75370911+akhilWrites@users.noreply.github.com>
2022-06-30 16:33:28 +0530

    updated

--- __tests__/handler.test.js
+++ __tests__/handler.test.js
@@ -1,4 +1,4 @@
-const handler = require('../handler');
+const handler = require('../index');
  
 test('correct greeting is generated', () => {
   expect(handler.printHelloWorld()).toBe("Hello World");
