95ea4a4cf29928fca4655a2a24c82a804f92d4c6
akhilWrites <75370911+akhilWrites@users.noreply.github.com>
2022-07-01 12:23:29 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports.helloworld = event => {
-  const randomNumber = parseInt(Math.random() * 100);
+  const randomNumber = parseInt(Math.random() * 10);
   console.log("The random generated integer is: ", randomNumber);
   return randomNumber;
 };
